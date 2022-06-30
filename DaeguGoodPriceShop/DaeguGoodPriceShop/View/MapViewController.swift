ae85180eed5e897eac6d12295ef43aa9b5818449
Cheong Jaeyun <59136391+imparang@users.noreply.github.com>
2022-06-30 11:59:18 +0900

    [Feature] #17 : Add userTrackingButton on map view

--- DaeguGoodPriceShop/DaeguGoodPriceShop/View/MapViewController.swift
+++ DaeguGoodPriceShop/DaeguGoodPriceShop/View/MapViewController.swift
@@ -11,8 +11,32 @@ import MapKit
 class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
+    private lazy var userTrackingButton: MKUserTrackingButton = {
+        let button = MKUserTrackingButton(mapView: mapView)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 10
+        button.clipsToBounds = true
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureView()
+    }
+    
+    func configureView() {
+        mapView.addSubview(userTrackingButton)
+        
+        NSLayoutConstraint.activate([
+            userTrackingButton.widthAnchor.constraint(equalToConstant: 50),
+            userTrackingButton.heightAnchor.constraint(equalToConstant: 50),
+            userTrackingButton.rightAnchor.constraint(equalTo: mapView.rightAnchor, constant: -50),
+            userTrackingButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -50)
+        ])
     }
 }
 
