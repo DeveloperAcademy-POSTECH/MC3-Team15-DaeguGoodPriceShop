d627f17ea29ab8686cc29058a3df1dc9119e6db7
Cheong Jaeyun <59136391+imparang@users.noreply.github.com>
2022-06-30 12:12:47 +0900

    [Feature] #16 : Append accumulation in zooming in and out on the map

--- DaeguGoodPriceShop/DaeguGoodPriceShop/View/MapViewController.swift
+++ DaeguGoodPriceShop/DaeguGoodPriceShop/View/MapViewController.swift
@@ -13,7 +13,7 @@ class MapViewController: UIViewController {
     
     private lazy var userTrackingButton: MKUserTrackingButton = {
         let button = MKUserTrackingButton(mapView: mapView)
-        
+        button.isHidden = false
         button.translatesAutoresizingMaskIntoConstraints = false
         button.backgroundColor = .white
         button.layer.cornerRadius = 10
@@ -26,6 +26,7 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         
         configureView()
+        configureMapView()
     }
     
     func configureView() {
@@ -34,9 +35,20 @@ class MapViewController: UIViewController {
         NSLayoutConstraint.activate([
             userTrackingButton.widthAnchor.constraint(equalToConstant: 50),
             userTrackingButton.heightAnchor.constraint(equalToConstant: 50),
-            userTrackingButton.rightAnchor.constraint(equalTo: mapView.rightAnchor, constant: -50),
-            userTrackingButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -50)
+            userTrackingButton.rightAnchor.constraint(equalTo: mapView.rightAnchor, constant: -30),
+            userTrackingButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -100)
         ])
+    }
+    
+    func configureMapView() {
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.showsScale = true
+        mapView.showsCompass = true
+        mapView.showsLargeContentViewer = true
     }
 }
 
+extension MapViewController: MKMapViewDelegate {
+    
+}
