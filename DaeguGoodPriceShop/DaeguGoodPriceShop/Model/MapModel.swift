f90c38441ac8d6b6714f51232f00382a765ed5d0
Yeongwoo-Poding <yw1221@g.skku.edu>
2022-07-05 11:21:51 +0900

    [Feature] #24 : Add Category Entity

--- DaeguGoodPriceShop/DaeguGoodPriceShop/Model/MapModel.swift
+++ DaeguGoodPriceShop/DaeguGoodPriceShop/Model/MapModel.swift
@@ -24,22 +24,26 @@ struct MapModel {
     private func getShopData() -> [Shop] {
         let fileName = "DaeguGoodPriceShop"
         guard let fileLocation = Bundle.main.url(forResource: fileName, withExtension: "json") else {
-            os_log(.error, log: .default, "\(DataFetchingError.invalidURL.localizedDescription)")
+            os_log(.error, log: .default, "INVALID URL: \(DataFetchingError.invalidURL.localizedDescription)")
             return []
         }
         
         guard let data = try? Data(contentsOf: fileLocation) else {
-            os_log(.error, log: .default, "\(DataFetchingError.urlUnableToConvertToData.localizedDescription)")
+            os_log(.error, log: .default, "URL UNABLE: \(DataFetchingError.urlUnableToConvertToData.localizedDescription)")
             return []
         }
         
         guard let shops = try? JSONDecoder().decode([Shop].self, from: data) else {
-            os_log(.error, log: .default, "\(DataFetchingError.unDecodable.localizedDescription)")
+            os_log(.error, log: .default, "UNDECODABLE: \(DataFetchingError.unDecodable.localizedDescription)")
             return []
         }
         
         os_log(.debug, log: .default, "Successfully Get Json Data")
         return shops
+    }
+    
+    func getCategorizedShop(category: ShopCategory) -> [Shop] {
+        return shops.filter{ category.isShopIncludedCategory(subCategory: $0.category) }
     }
 }
 
