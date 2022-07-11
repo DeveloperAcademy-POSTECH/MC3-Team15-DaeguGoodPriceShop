20413645f8aaa59559c5c14f84263cbb37fc9eaa
Jin-won Choi(Kelly Chui) <optioneldar@gmail.com>
2022-07-12 05:09:18 +0900

    [Feature] #47 : Merge & Rebase

--- DaeguGoodPriceShop/DaeguGoodPriceShop/View/ModalViewController/DetailModalViewController.swift
+++ DaeguGoodPriceShop/DaeguGoodPriceShop/View/ModalViewController/DetailModalViewController.swift
@@ -28,10 +28,10 @@ class DetailModalViewController: ModalViewController {
         var modalTitleView: UIStackView = {
             let label = UILabel()
             label.text = "착한 짜장"
-            label.font = .boldSystemFont(ofSize: 32)
+            label.font = .boldSystemFont(ofSize: 24)
             
             let textView = UITextView(frame: .zero)
-            textView.font = UIFont.systemFont(ofSize: 16)
+            textView.font = UIFont.systemFont(ofSize: 15)
             textView.isEditable = false
             textView.text = "❤️"
             //textView.trailingAnchor.constraint(equalTo: modalTitleView.leadingAnchor).isActive = true
@@ -46,31 +46,35 @@ class DetailModalViewController: ModalViewController {
         
         var modalMenuView: UIStackView = {
             let menuName = UITextView(frame: .zero)
-            menuName.font = UIFont.systemFont(ofSize: 16)
+            menuName.font = UIFont.systemFont(ofSize: 15)
             menuName.isEditable = false
             menuName.text = "짜장면" // TODO: Data
             menuName.isScrollEnabled = false
+            menuName.backgroundColor = .clear
             
             let menuPrice = UITextView(frame: .zero)
-            menuPrice.font = UIFont.systemFont(ofSize: 16)
+            menuPrice.font = UIFont.systemFont(ofSize: 15)
             menuPrice.isEditable = false
             menuPrice.text = "5000원" // TODO: Data
             menuPrice.isScrollEnabled = false
+            menuPrice.backgroundColor = .clear
             
             let spacer = UIView()
             
             let menuStack = UIStackView(arrangedSubviews: [menuName, spacer, menuPrice])
             menuStack.axis = .horizontal
             menuStack.spacing = 16.0
+            menuStack.customizeBackground(color: .systemGray6, radiusSize: 5)
             return menuStack
         }()
         var modalInfoView: UIStackView = {
             var modalAddressView: UIStackView = {
                 let infoAddress = UITextView(frame: .zero)
-                infoAddress.font = UIFont.systemFont(ofSize: 16)
+                infoAddress.font = UIFont.systemFont(ofSize: 15)
                 infoAddress.isEditable = false
                 infoAddress.text = "대구광역시 주소구 주소로 123" // TODO: Data
                 infoAddress.isScrollEnabled = false
+                infoAddress.backgroundColor = .clear
                 
                 let infoSymbolCopy = UIImageView()
                 infoSymbolCopy.image = UIImage(systemName: "doc.on.doc.fill")
@@ -81,19 +85,16 @@ class DetailModalViewController: ModalViewController {
                 infoAddressStack.axis = .horizontal
                 infoAddressStack.spacing = 16.0
                 
-                NSLayoutConstraint.activate([
-                    infoAddress.heightAnchor.constraint(equalToConstant: 30.0)
-                ])
-                
                 return infoAddressStack
             }()
             var modalPhoneView: UIStackView = {
                 let infoPhoneNumber = UITextView(frame: .zero)
-                infoPhoneNumber.font = UIFont.systemFont(ofSize: 16)
+                infoPhoneNumber.font = UIFont.systemFont(ofSize: 15)
                 infoPhoneNumber.isEditable = false
                 infoPhoneNumber.text = "010-0000-0000" // TODO: Data
                 infoPhoneNumber.textAlignment = .center
                 infoPhoneNumber.isScrollEnabled = false
+                infoPhoneNumber.backgroundColor = .clear
                 
                 let infoSymbolPhone = UIImageView()
                 infoSymbolPhone.image = UIImage(systemName: "phone.fill")
@@ -104,53 +105,55 @@ class DetailModalViewController: ModalViewController {
                 infoPhoneStack.axis = .horizontal
                 infoPhoneStack.spacing = 16.0
                 
-                NSLayoutConstraint.activate([
-                    infoPhoneNumber.heightAnchor.constraint(equalToConstant: 30.0)
-                ])
-                
                 return infoPhoneStack
             }()
             let infoSubTitleAddress = UITextView(frame: .zero)
-            infoSubTitleAddress.font = UIFont.systemFont(ofSize: 16)
+            infoSubTitleAddress.font = UIFont.systemFont(ofSize: 15)
             infoSubTitleAddress.isEditable = false
             infoSubTitleAddress.text = "주소"
             infoSubTitleAddress.isScrollEnabled = false
+            infoSubTitleAddress.backgroundColor = .clear
+            
+            let divider = UIView()
+            divider.backgroundColor = .systemGray3
+            divider.widthAnchor.constraint(equalToConstant: 100.0).isActive = true
+            divider.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
             
             let infoSubTitlePhone = UITextView(frame: .zero)
-            infoSubTitlePhone.font = UIFont.systemFont(ofSize: 16)
+            infoSubTitlePhone.font = UIFont.systemFont(ofSize: 15)
             infoSubTitlePhone.isEditable = false
             infoSubTitlePhone.text = "전화번호"
             infoSubTitlePhone.isScrollEnabled = false
+            infoSubTitlePhone.backgroundColor = .clear
             
-            let infoStack = UIStackView(arrangedSubviews: [infoSubTitleAddress, modalAddressView, infoSubTitlePhone, modalPhoneView])
+            let infoStack = UIStackView(arrangedSubviews: [infoSubTitleAddress, modalAddressView, divider ,infoSubTitlePhone, modalPhoneView])
             infoStack.axis = .vertical
-            infoStack.spacing = 16.0
-            infoStack.addBackground(color: .systemGray)
+            infoStack.spacing = 0
+            infoStack.customizeBackground(color: .systemGray6, radiusSize: 10.0)
             
             return infoStack
         }()
         let subTitleMenu = UILabel()
         subTitleMenu.text = "대표 메뉴"
-        subTitleMenu.font = .boldSystemFont(ofSize: 20)
+        subTitleMenu.font = .boldSystemFont(ofSize: 15)
         
         let subTitleInfo = UILabel()
         subTitleInfo.text = "가게 정보"
-        subTitleInfo.font = .boldSystemFont(ofSize: 20)
+        subTitleInfo.font = .boldSystemFont(ofSize: 15)
         
         let detailStack = UIStackView(arrangedSubviews: [modalTitleView, subTitleMenu, modalMenuView, subTitleInfo, modalInfoView])
         detailStack.spacing = 16.0
         detailStack.axis = .vertical
         
-        /*
-        NSLayoutConstraint.activate([
+        
+        /*        NSLayoutConstraint.activate([
             modalTitleView.heightAnchor.constraint(equalToConstant: 40.0),
             modalMenuView.leadingAnchor.constraint(equalTo: modalTitleView.leadingAnchor),
             modalInfoView.leadingAnchor.constraint(equalTo: modalTitleView.leadingAnchor),
             subTitleMenu.leadingAnchor.constraint(equalTo: modalTitleView.leadingAnchor),
             subTitleInfo.leadingAnchor.constraint(equalTo: modalTitleView.leadingAnchor)
         ])
-         */
-        
+        */
         return detailStack
     }()
 
@@ -195,7 +198,7 @@ class DetailModalViewController: ModalViewController {
             favoriteButton.topAnchor.constraint(equalTo: dismissButton.topAnchor),
             favoriteButton.trailingAnchor.constraint(equalTo: dismissButton.leadingAnchor),
             
-            modalDetailView.widthAnchor.constraint(equalToConstant: self.view.frame.width),
+            modalDetailView.widthAnchor.constraint(equalToConstant: self.view.frame.width - 30.0),
             modalDetailView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             modalDetailView.topAnchor.constraint(equalTo: gestureView.bottomAnchor, constant: -30)
         ])
@@ -254,11 +257,19 @@ class DetailModalViewController: ModalViewController {
 }
 
 extension UIStackView {
-    func addBackground(color: UIColor) {
+    func customizeBackground(color: UIColor, radiusSize: CGFloat = 0.0) {
         let subView = UIView(frame: bounds)
         subView.backgroundColor = color
         subView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         insertSubview(subView, at: 0)
+        
+        subView.layer.cornerRadius = radiusSize
+        subView.layer.masksToBounds = true
+        subView.clipsToBounds = true
     }
 }
-
+/*
+extension UIColor {
+    class var Gray
+}
+*/
